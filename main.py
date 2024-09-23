d7844d052576911c09c9541ca440fe3baab6ce8b
shiying <shiying.wei@ucl.ac.uk>
2024-09-23 16:17:45 +0100

    added CRUD methods(not completed)

--- main.py
+++ main.py
@@ -2,41 +2,64 @@
 #Pickle
 from typing import List
 import pickle
-from dataclasses import dataclass
+from dataclasses import dataclass,field
 
 #data class
+
 @dataclass
 class Person:
     name: str
     age: int
     contact: str
-
+        
 @dataclass
-class Candidates():
-    candidates: List[Person]
+class Candidates:
+    position: str
+    people: list[Person] = field(default_factory=list) #creates an empty list for all the Person classes
 
-candidates=[]
+    #CRUD
+    def add_person(self, person:Person):
+        self.people.append(person)
+        with open("candidate"+str(len(self.people))+".pkl","wb") as file:
+            pickle.dump(self,file) 
 
+    def read_people(self):
+        with open("candidate"+str(len(self.people))+".pkl","rb") as file:
+            loaded_candidate = pickle.load(file)
+
+            print(loaded_candidate)
+            
+        #print(f"Candidate name: {person.name} , age: {person.age} , contact details: {person.contact} ")
+
+    def update_person(self):
+        pass
+
+    def delete_person(self):
+        self.people.remove(self)
+        
+# candidate positions
+crew = Candidates(position="Krabby Patty employee")
+
+#person objects
 spongebob = Person(name="Spongebob",age=30,contact="7238479825")
 patrick = Person(name="Patrick",age=24,contact="2347929348")
+squidward = Person(name="Squidward",age=98,contact="2784279384")
 
+#tests
+crew.add_person(spongebob)
+crew.add_person(patrick)
+print(len(crew.people))
 
-#objects
-
-
-#CRUD
-'''def create_candidate():
-    candidate = Candidate(name,age,contact)
-    return'''
-
+crew.read_people()
 #pickle
-with open("candidate.pkl","wb") as file:
+
+"""with open("candidate.pkl","wb") as file:
     pickle.dump(spongebob,file)
 
 with open("candidate.pkl","rb") as file:
     loaded_candidate = pickle.load(file)
 
-print(loaded_candidate)
+print(loaded_candidate)"""
 
 
 #
