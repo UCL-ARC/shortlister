f18fc39518506cd61404fecd9004481e589f370a
shiying <shiying.wei@ucl.ac.uk>
2024-09-24 12:20:42 +0100

    before separating models and controllers

--- main.py
+++ main.py
@@ -12,16 +12,30 @@ class Person:
     age: int
     contact: str
         
+    def  update_person(self):
+            choice = input(f"What do you want to update for {self.name} ? (name, age or contact?):")
+            if choice.lower() == "name":
+                new_name = input("Please enter the new name:")
+                print(f"Updated {self.name}'s grade to {new_name}")
+            if choice.lower() == "age":
+                new_age = input("Please enter the new age:")
+                print(f"Updated {self.age}'s grade to {new_age}")
+            if choice.lower() == "contact":
+                new_contact = input("Please enter the new contact:")
+                print(f"Updated {self.contact}'s grade to {new_contact}")
+            else:
+                pass
+
 @dataclass
 class Candidates:
     position: str
-    people: list[Person] = field(default_factory=list) #creates an empty list for all the Person classes
+    people: List[Person] = field(default_factory=list) #creates an empty list for all the Person classes
 
     #CRUD
     def add_person(self, person:Person):
         self.people.append(person)
         with open("candidate"+str(len(self.people))+".pkl","wb") as file:
-            pickle.dump(self,file) 
+            pickle.dump(self.people,file) 
 
     def read_people(self):
         with open("candidate"+str(len(self.people))+".pkl","rb") as file:
@@ -29,13 +43,12 @@ class Candidates:
 
             print(loaded_candidate)
             
-        #print(f"Candidate name: {person.name} , age: {person.age} , contact details: {person.contact} ")
+        #print(f"Candidate name: {person(.name} , age: {person.age} , contact details: {person.contact} ")
 
-    def update_person(self):
-        pass
 
-    def delete_person(self):
-        self.people.remove(self)
+
+    def delete_person(self,person):
+        self.people.remove(person)
         
 # candidate positions
 crew = Candidates(position="Krabby Patty employee")
@@ -48,7 +61,8 @@ squidward = Person(name="Squidward",age=98,contact="2784279384")
 #tests
 crew.add_person(spongebob)
 crew.add_person(patrick)
-print(len(crew.people))
+crew.people.remove(patrick)
+print(crew.people)
 
 crew.read_people()
 #pickle
@@ -62,4 +76,4 @@ with open("candidate.pkl","rb") as file:
 print(loaded_candidate)"""
 
 
-#
+#repl
