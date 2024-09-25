1cd792689d63566c6a574f9221c54f98c91db0b6
shiying <shiying.wei@ucl.ac.uk>
2024-09-25 13:11:26 +0100

    add test for creating criterion objects from csv file

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from model import Applicant, Criterion, Role, Shortlist
+import csv
 
 def test_model():
     c1 = Criterion("PhD degree","degree or experience",["E","S","M","U"])
@@ -9,4 +10,27 @@ def test_model():
     s = Shortlist(r,[a1,a2])
     print(s)
 
-test_model()
+def test_read_criteria():
+    csv_file = "test_role/criteria.csv"
+    with open(csv_file,"r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+
+def test_create_criterion():
+    csv_file = "test_role/criteria.csv"
+    with open(csv_file) as file:
+        reader= csv.reader(file)
+
+        criteria = []
+
+        for row in reader:
+            criterion = Criterion(row[0],row[1],[])
+            criteria.append(criterion)
+        print(criteria)
+
+test_create_criterion()
+
+#csv library
+# use csv library to read the criteria.csv and create criterion objects based on the file contents
+#open file and step through each rows
