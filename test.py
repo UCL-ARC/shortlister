8fff3038d1d3d1c42b7c6df9e145fc93c33dcc65
shiying <shiying.wei@ucl.ac.uk>
2024-09-26 13:31:55 +0100

    add test for creating applicant objects from file name

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+from typing import List
 from model import Applicant, Criterion, Role, Shortlist
 import csv
+import glob
 
 def test_model():
     c1 = Criterion("PhD degree","degree or experience",["E","S","M","U"])
@@ -30,11 +32,27 @@ def test_create_criterion(csv_file:str):
         return(criteria)
 
 def test_create_applicant():
-    applicant = Applicant()
+    files = glob.glob("test_role/*.pdf")
+    applicants = []
+    for file in files:
+        rm_extension = file.rsplit(".")[0] #splits and leaves only index 0 (removes .pdf)
+        base_name = rm_extension.rsplit("\\")[1] 
+        name_parts = base_name.split("_") #splits the whole string using delimiter "_""
+        full_name = name_parts[0] + " " +name_parts[1]
+        applicant = Applicant(full_name,files,{})
+        applicants.append(applicant)
+    print(applicants)
 
 
 
-print(test_create_criterion("test_role/criteria.csv")[0])
+
+    
+    #using delimiter "_" to separate 
+# import a directory, find all pdf files in that directory, split the files names, return the files names, and create applicant objects with file names  
+
+test_create_applicant()
+
+#print(test_create_criterion("test_role/criteria.csv")[0])
 #csv library
 # use csv library to read the criteria.csv and create criterion objects based on the file contents
 #open file and step through each rows
