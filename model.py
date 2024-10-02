c357498b8bc1ed56da49631c3243e4b6370f641c
shiying <shiying.wei@ucl.ac.uk>
2024-10-02 13:12:24 +0100

    separate dataclass and functions

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import glob
+from pathlib import Path
 from typing import Dict, List
 from dataclasses import dataclass
 import csv
@@ -20,20 +22,51 @@ class Role:
     job_id: str
     criteria: List[Criterion]
 
-    def create_criteria(self,csv_file):
-         with open(csv_file) as file:
-            reader= csv.reader(file)
-            next(reader)
-
-            for row in reader:
-                criterion = Criterion(row[0],row[1],[])
-                self.criteria.append(criterion)
-            return(self.criteria)
 
 @dataclass
 class Shortlist:
     role: Role
     applicants: List[Applicant]
 
-    def creat_role(self,role):
-        pass
+
+#functions
+
+def load_shortlist(path):
+    criteria = load_criteria(path+"/criteria.csv")
+    role = load_role(path,criteria)
+    applicants = load_applicants(path)
+    shortlist = Shortlist(role,applicants)
+    return shortlist
+
+def load_role(path,criteria):
+    role = (path,"0001",criteria)
+    return role
+    
+
+
+def load_applicants(path):
+    p = Path(path)
+    files = glob.glob(str(p/"*.pdf"))
+    applicants = []
+    for file in files:
+        file = Path(file)
+        name_parts = file.stem.split("_")#removes .pdf 
+
+       # type(" ".join(name_parts[0,1]))
+        applicant = Applicant(" ".join(name_parts[0:2]),file,{})
+        applicants.append(applicant)
+    return applicants
+
+def load_criteria(csv_file):
+    criteria = []
+    with open(csv_file) as file:
+        reader= csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            criterion = Criterion(row[0],row[1],[])
+            criteria.append(criterion)
+    return criteria
+         
+def get_role_info(self):
+    return self.job_title,self.job_id,self.criteria
