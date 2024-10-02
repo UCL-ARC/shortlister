5ec8d124cb1699f6f2a3ab570eb692d4365547c6
shiying <shiying.wei@ucl.ac.uk>
2024-10-02 18:24:32 +0100

    improve readability of view applicants

--- view.py
+++ view.py
@@ -9,7 +9,15 @@ class View:
         print(f"The role is:{role}")
 
     def view_applicants(self,applicants):
-        print(f"Here are all of the applicants:{applicants}")
+        print(f"Here are all of the applicants:")
+        print()
+
+        for applicant in applicants:
+          index = applicants.index(applicant)
+          print(f"{index+1}.Name: {applicant.name}")
+          print(f"CV Path: {applicant.cv}")
+          print(f"Scores: {applicant.scores}")
+          print()
     
     def view_shortlist(self,shortlist):
         print(shortlist)
