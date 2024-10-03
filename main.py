e68e8635c358730d66ebf1c8379df21da4d20a03
shiying <shiying.wei@ucl.ac.uk>
2024-10-03 15:43:07 +0100

    fix TypeError when running the script and add comments

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from controller import *
 import argparse
 
+#this creates an instance of parser
 parser = argparse.ArgumentParser(description="loads shortlist data from role directory")
 
+#creates some argument you can use when running the file
 parser.add_argument("rolepath",type=str)
 
+# set the value of the argument parsed in so it can be accessed later 
 args = parser.parse_args()
 
-control = Controller(args)
+# accesses the rolepath from argument parsed
+control = Controller(args.rolepath)
+
+control.show_criteria()
