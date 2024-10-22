2fee29d833c4df334a8e460a033316403dc29e6c
shiying <shiying.wei@ucl.ac.uk>
2024-10-22 12:12:52 +0100

    remove elif option blocks

--- controller.py
+++ controller.py
@@ -10,16 +10,20 @@ class Controller:
         self.shortlist = load_shortlist(path)
         self.current_applicant = None
         self.current_criterion = None
-        self.current_view = "home"
         self.view = View()
         self.options = None # intial state
-        self.options_1 = {"0":self.options_2a, # list of criteria
-                          "1":self.options_2b,}
+        self.options_home = {"r":self.show_role_info,
+                            "a":self.show_applicants_list}
+        self.options_applist = {"b":self.show_boot_message,
+                                 "d":self.show_applicant_details}
+        self.options_appdetail = {"a":self.show_applicants_list,
+                                   "e":self.edit_applicant_score,
+                                   "b":self.show_boot_message,
+                                   "O":self.open_applicant_pdf}
 
     def edit_appscore_start(self,k=None):
         print("Select an criteria to edit score for:")
-        self.options = self.options_1
-        print(self.options)
+        self.options = None
 
     def edit_criteria_select(self, k=None):
         print("You selected A. Select more...")
@@ -37,42 +41,40 @@ class Controller:
         print(f"You selected {k}. Back to start...")
         self.options = self.step1_options
     
-    def show_boot_message(self):
+    def show_boot_message(self,k=None):
         """Shortlist overview"""
         self.view.boot_message(self.path,len(self.shortlist.applicants))
-        self.current_view = "home"
+        self.options = self.options_home
 
-    def show_criteria(self):
+    def show_criteria(self,k=None):
         """Display criteria information"""
         self.view.view_criteria(self.shortlist.role,self.shortlist.role.criteria)
-        self.current_view = "criteria"
 
-    def show_role_info(self):
+    def show_role_info(self,k=None):
         """Display role information"""
         self.view.view_role(self.shortlist.role)
-        self.current_view = "role"
 
-    def show_applicants_list(self):
+    def show_applicants_list(self,k=None):
         """List all applicants"""
         self.view.view_applicants_list(self.shortlist)
-        self.current_view = "applicants_list"
+        self.options = self.options_applist
 
-    def show_applicant_details(self):
+    def show_applicant_details(self,k=None):
         """Select an applicant and view details"""
         try:
             i = int(input("Please enter the applicant number:"))
 
             self.current_applicant = self.shortlist.applicants[i-1]
             self.view.view_applicant_details(self.current_applicant)
-            self.current_view = "applicant_details"
+            self.options = self.options_appdetail
         except (ValueError, IndexError):
             pass
 
-    def open_applicant_pdf(self):
+    def open_applicant_pdf(self,k=None):
         """Open current applicant's CV"""
         startfile(self.current_applicant.cv)
         
-    def edit_applicant_score(self):
+    def edit_applicant_score(self,k=None):
         """View and score applicants based on criteria"""
         print("Select the criteria you would like to mark:")
 
@@ -110,36 +112,18 @@ class Controller:
 
         while True:
             k = readkey()
+
             if k == "q":
                 print("exiting the program...")
                 break
 
-            elif self.current_view == "home":
-                options = {"r":self.show_role_info,
-                           "a":self.show_applicants_list}
-                
-            elif self.current_view == "applicants_list":
-                options = {"b":self.show_boot_message,
-                           "d":self.show_applicant_details}
-                
-            elif self.current_view == "applicant_details":
-                options = {"a":self.show_applicants_list,
-                           "e":self.edit_applicant_score,
-                           "b":self.show_boot_message,
-                           "O":self.open_applicant_pdf}
-
-            elif self.current_view == "role":
-                options = {"b":self.show_boot_message}
-                
-            elif self.current_view == "criteria":
-                options = {"b":self.show_boot_message}
+            if k == "?":
+                print("---List of shortcuts---")
+                print("q: Exit the program")
+                for keypress,func in self.options.items():
+                    print(f"{keypress}: {func.__doc__}")
             
-            if options is not None:
-                output = options.get(k)
-                if k == "?":
-                    print("---List of shortcuts---")
-                    print("q: Exit the program")
-                    for keypress,func in options.items():
-                        print(f"{keypress}: {func.__doc__}")
-                elif output is not None:
+            else :
+                output = self.options.get(k)
+                if output is not None:
                     output(k=k)
