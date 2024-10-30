9fca1465b15e57b22a670c08dc46a51efe25d52c
shiying <shiying.wei@ucl.ac.uk>
2024-10-30 19:41:54 +0000

    experimenting with parametrized tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,10 +8,12 @@ def test_load_criteria(filepath= "test_role/criteria.csv"):
         assert type(criterion) is expected
         assert criterion.name and criterion.description and criterion.scores is not None
     ...
-
-def test_load_applicants():
-    result = mdl.load_applicants
-    pass
+@pytest.mark.parametrize("folder_path,expected",
+                         "test_role",["Emma Jones","Michael Davis","Sarah Thompson"])
+def test_load_applicants(folder_path,expected):
+    applicants = mdl.load_applicants(folder_path)
+    result = [applicant.name for applicant in applicants]
+    assert result == expected
 
 def test_load_role():
     ...
