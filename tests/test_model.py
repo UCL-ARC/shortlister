527dee081dc89d63645b814f3f1a8d5c4c1e236b
shiying <shiying.wei@ucl.ac.uk>
2024-10-31 12:06:40 +0000

    correct syntax error

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,7 +9,7 @@ def test_load_criteria(filepath= "test_role/criteria.csv"):
         assert criterion.name and criterion.description and criterion.scores is not None
     ...
 @pytest.mark.parametrize("folder_path,expected",
-                         "test_role",["Emma Jones","Michael Davis","Sarah Thompson"])
+                         [("test_role",["Emma Jones","Michael Davis","Sarah Thompson"])])
 def test_load_applicants(folder_path,expected):
     applicants = mdl.load_applicants(folder_path)
     result = [applicant.name for applicant in applicants]
