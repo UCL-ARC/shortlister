eaf3b403ba496e9245c0dad0b388f452049d5660
shiying <shiying.wei@ucl.ac.uk>
2024-10-31 16:37:09 +0000

    module not found error when loading pickle

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,14 @@
 import pytest
 import source_code.model as mdl
+from pathlib import Path
 
 path = "test_role"
+pickle_file_name = "shortlist.pickle"
 
 def test_load_criteria(filepath= "test_role/criteria.csv"):
-    result = mdl.load_criteria(filepath)
+    criteria_result = mdl.load_criteria(filepath)
     expected = mdl.Criterion
-    for criterion in result:
+    for criterion in criteria_result:
         assert type(criterion) is expected
         assert criterion.name and criterion.description and criterion.scores is not None
     ...
@@ -21,8 +23,10 @@ def test_load_applicants(folder_path,expected):
 def test_load_role():
     result = mdl.load_role(path,test_load_criteria)
     expected = "test_role"
+
     assert result.job_title == expected
 
-def test_load_shortlist():
-    shortlist = mdl.load_shortlist(path)
-    assert type(shortlist) is mdl.Shortlist
+def test_load_pickle():
+    result = mdl.load_pickle(Path(path)/pickle_file_name)
+    assert result is mdl.Shortlist
+    #not sure why it returns module not found error 
