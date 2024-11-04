131053cb39918e5352f48182da748ba2ad52b333
shiying <shiying.wei@ucl.ac.uk>
2024-11-04 14:14:47 +0000

    add test for creating controller instance

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,2 +1,19 @@
-import shortlister.controller as ctrl
+from shortlister.controller import Controller
 from pathlib import Path
+
+controller = Controller(path=Path("test_role"))
+
+def test_create_controller():
+    result = []
+    for applicant in controller.shortlist.applicants:
+        result.append(applicant.name)
+    expected = ["Emma Jones","Michael Davis","Sarah Thompson"]
+
+    assert result == expected
+        
+
+def test_keycapture():
+    controller.run()
+
+    
+
