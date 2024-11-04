25b6ba34edfab87d1cacd873995f5a2984a665ec
shiying <shiying.wei@ucl.ac.uk>
2024-11-04 15:37:43 +0000

    rename mdl to model for better readability

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,13 @@
 import pytest
-import shortlister.model as mdl
+from  shortlister import model
 from pathlib import Path
 
 path = "test_role"
 pickle_file_name = "shortlist.pickle"
 
 def test_load_criteria(filepath= "test_role/criteria.csv"):
-    criteria_result = mdl.load_criteria(filepath)
-    expected = mdl.Criterion
+    criteria_result = model.load_criteria(filepath)
+    expected = model.Criterion
     for criterion in criteria_result:
         assert type(criterion) is expected
         assert criterion.name and criterion.description and criterion.scores is not None
@@ -16,43 +16,43 @@ def test_load_criteria(filepath= "test_role/criteria.csv"):
                          [("test_role",["Emma Jones","Michael Davis","Sarah Thompson"]),
                           ("non_existing_folder",[])])
 def test_load_applicants(folder_path,expected):
-    applicants = mdl.load_applicants(folder_path)
+    applicants = model.load_applicants(folder_path)
     result = [applicant.name for applicant in applicants]
     assert result == expected
 
 def test_load_role():
-    criteria = [mdl.Criterion(name="PhD",
+    criteria = [model.Criterion(name="PhD",
                               description="Degree or relevant experience",
                               scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent")),
-                mdl.Criterion(name="Research software",
+                model.Criterion(name="Research software",
                               description="Authorship,development and maintenance",
                               scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent")),
-                mdl.Criterion(name="Best practices",
+                model.Criterion(name="Best practices",
                               description="Issue tracking, testing, documentation etc.",
                               scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent"))]
-    result = mdl.load_role(path,criteria)
+    result = model.load_role(path,criteria)
     
 
-    expected = mdl.Role(job_title="test_role",
+    expected = model.Role(job_title="test_role",
                         job_id="0001",
                         criteria=criteria)
 
     assert result == expected
 
 def test_save_load():
-    expected = mdl.Shortlist(role= mdl.Role(job_title="test_role",
+    expected = model.Shortlist(role= model.Role(job_title="test_role",
                                              job_id="0000",
                                              criteria=[]),
-                              applicants=[mdl.Applicant(name="George Smith",
+                              applicants=[model.Applicant(name="George Smith",
                                                         cv="placeholder",
                                                         scores=[]),
-                                          mdl.Applicant(name = "Jim Chapman",
+                                          model.Applicant(name = "Jim Chapman",
                                                         cv="placeholder",
                                                         scores=[])]
                                                         )
     
-    mdl.save_shortlist(path=Path("tests"), shortlist=expected)
-    result:mdl.Shortlist = mdl.load_pickle("tests/shortlist.pickle")
+    model.save_shortlist(path=Path("tests"), shortlist=expected)
+    result:model.Shortlist = model.load_pickle("tests/shortlist.pickle")
 
     assert result.role.job_title == "test_role"
     assert result.role.job_id == "0000"
