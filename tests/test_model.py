7b44ce3d7bba0a55115ab119569028d367046bdb
shiying <shiying.wei@ucl.ac.uk>
2024-11-06 12:00:22 +0000

    use dummy data for tests and add check for applicant score criterion in criteria

--- tests/test_model.py
+++ tests/test_model.py
@@ -23,25 +23,6 @@ def test_load_applicants(folder_path,expected):
 
 def test_load_role():
     criteria = [model.Criterion(name="PhD",
-                              description="Degree or relevant experience",
-                              scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent")),
-                model.Criterion(name="Research software",
-                              description="Authorship,development and maintenance",
-                              scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent")),
-                model.Criterion(name="Best practices",
-                              description="Issue tracking, testing, documentation etc.",
-                              scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent"))]
-    result = model.load_role(path,model.load_criteria(path/csv_file))
-    
-
-    expected = model.Role(job_title="test_role",
-                        job_id="0001",
-                        criteria=criteria)
-
-    assert result == expected
-
-def test_save_load():
-    c = [model.Criterion(name="PhD",
                          description="Degree or relevant experience",
                          scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent")),
          model.Criterion(name="Research software",
@@ -51,29 +32,54 @@ def test_save_load():
                          description="Issue tracking, testing, documentation etc.",
                          scores = ("Unsatisfactory","Moderate","Satisfactory","Excellent"))]
     
-    a = [model.Applicant(name="George Smith",
-                         cv="placeholder",
+    result = model.load_role(path,model.load_criteria(path/csv_file))
+    expected = model.Role(job_title="test_role",
+                        job_id="0001",
+                        criteria=criteria)
+
+    assert result == expected
+
+def test_save_load():
+
+    s = ("1","2","3","4")
+    c = [model.Criterion(name="c1",
+                        description="d1",
+                        scores = s),
+        model.Criterion(name="c2",
+                        description="d2",
+                        scores = s),
+        model.Criterion(name="c3",
+                        description="d3",
+                        scores = s)]
+    
+    a = [model.Applicant(name="a1",
+                         cv="c1",
                          scores={c[0]:c[0].scores[3],
                                  c[1]:c[1].scores[2],
                                  c[2]:c[2].scores[0]}),
-         model.Applicant(name="Jim Chapman",
-                         cv="placeholder",
+         model.Applicant(name="a2",
+                         cv="c2",
                          scores={c[0]: c[0].scores[1],
                                  c[1]: c[1].scores[0]})]
 
     shortlist = model.Shortlist(role= model.Role(job_title="tests",
                                                 job_id="0000",
                                                 criteria=c),
-                               applicants=a)
+                                                applicants=a)
     
     model.save_shortlist(Path("tests"), shortlist)
     result:model.Shortlist = model.load_pickle(Path("tests")/pickle_file_name)
 
+    applicant_criterion_list = [criterion for applicant in result.applicants for criterion in list(applicant.scores.keys())]
+    
+    for criterion in applicant_criterion_list:
+        assert criterion in c
     assert result.role.job_title == "tests"
     assert result.role.job_id == "0000"
     assert result.role.criteria == c
     assert result.applicants == a 
-    assert result.applicants[0].name == "George Smith"
-    assert result.applicants[1].name == "Jim Chapman"
+    assert result.applicants[0].name == "a1"
+    assert result.applicants[1].name == "a2"
+    
 
     
