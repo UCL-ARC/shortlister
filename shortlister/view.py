013d42b16e0becbeba81c379189a4c590be804c8
Asif Tamuri <tamuri@gmail.com>
2024-11-13 16:38:20 +0000

    Bit of tidying up/simplifcation

--- shortlister/view.py
+++ shortlister/view.py
@@ -6,20 +6,39 @@ class View:
     def __init__(self):
         return
 
-    def boot_message(self, role, num_applicants):
+    def title(self):
+        print("""
+     _                _   _ _     _            
+ ___| |__   ___  _ __| |_| (_)___| |_ ___ _ __ 
+/ __| '_ \ / _ \| '__| __| | / __| __/ _ \ '__|
+\__ \ | | | (_) | |  | |_| | \__ \ ||  __/ |   
+|___/_| |_|\___/|_|   \__|_|_|___/\__\___|_|
+
+""")
+
+    def home_message(self, role, num_applicants):
         """Prints overview of shortlist to console."""
-        print(f"\nShortlist for {role} loaded:")
+        print(f"Shortlist for {role} loaded:")
         print(f"{num_applicants} applicants found")
         print("('?' to show list of avaliable shortcuts)")
+        print()
 
     def view_role(self, role: Role):
         """Prints overview of role to console."""
-        print(f"\nRole title: {role.job_title}")
+        print(f"Role title: {role.job_title}")
         print(f"Role ID: {role.job_id}")
+        print()
 
-    def view_applicant_details(self, applicant: Applicant,criteria: List[Criterion], total_score,applicant_number,total_applicant):
+    def view_applicant_details(
+        self,
+        applicant: Applicant,
+        criteria: List[Criterion],
+        total_score,
+        applicant_number,
+        total_applicant,
+    ):
         """Prints details of applicant to console."""
-        print(f"**{applicant_number}/{total_applicant} {applicant.name}**")
+        print(f"** {applicant_number}/{total_applicant} {applicant.name} **")
         print(f"CV Path: {applicant.cv}")
 
         if applicant.scores:
@@ -32,14 +51,14 @@ class View:
 
         if applicant.notes:
             print(f"Notes: {applicant.notes}\n")
-        else:
-            print()
+
+        print()
 
     def view_applicants_list(self, shortlist: Shortlist):
         """Prints list of all applicants to console."""
-        print("\nList of applicants:")
         for index, applicant in enumerate(shortlist.applicants):
             print(f"{index+1}. {applicant.name}")
+        print()
 
     def view_criteria(self, role: Role, criteria: list[Criterion]):
         """Prints list of all criterion for the role to console."""
@@ -49,15 +68,20 @@ class View:
         for index, criterion in enumerate(criteria):
             print(f"{index}. {criterion.name}: {criterion.description}")
 
-    def view_selection_options(self, criterion: Criterion,):
+    def view_selection_options(
+        self,
+        criterion: Criterion,
+    ):
         """Prints list of avaliable scoring option for selected criterion to console."""
         print(
-            f"\nYou selected {criterion.name}. Select the score you want to change to:\n"
+            f"You selected {criterion.name}. Select the score you want to change to:\n"
         )
 
         for index, score in enumerate(criterion.scores):
             print(f"{index}: {score}")
         print("c: Clear scores\n")
+        print()
 
     def view_update(self, attribute, change):
         print(f"Updated: {attribute} to: {change}\n")
+        print()
