29fd08eabaa50a24ad2dea6af31a6cfa2f6e4dad
shiying <shiying.wei@ucl.ac.uk>
2024-11-28 15:35:44 +0000

    Rework pairing

--- shortlister/tournament.py
+++ shortlister/tournament.py
@@ -1,4 +1,4 @@
-from random import choice
+
 
 RESULT= {}
 
@@ -8,9 +8,16 @@ def comparison(list):
     save_results(pair,winner)
 
 def get_pair(object_list):
-    object_1 = choice(object_list)
-    object_2 = choice(object_list) 
-    return (object_1, object_2)
+    """get every possible item pair in the list"""
+    pairing = []
+    for item in object_list:
+        comparison_list = [opponent for opponent in object_list if not item]
+        for opponent in comparison_list:
+            if item and opponent in RESULT.keys():
+                continue
+            else:
+                pairing.append(set(item,opponent))
+    return pairing
 
 def choose(candidates:tuple):
     choice = input()
@@ -22,3 +29,22 @@ def choose(candidates:tuple):
 
 def save_results(pair,winner):
     RESULT[set(pair)] = winner
+
+def rank(list):
+    # checking from result where the winners should be placed
+    # if object1 is winner, then it should be placed above object2 and all other objects that object2 won against ect.
+    # 
+    for key,value in RESULT:
+        if key[value] == value:
+            ...
+
+
+    # rank placement
+
+    
+# Ranking
+
+# elo system?
+# total number of applicants
+# imported list is sorted based on score
+# a vs b, b vs c, c vs
