a58f059062cdf9eab9fc14c7498bdf92a44f2a90
shiying <shiying.wei@ucl.ac.uk>
2024-12-02 13:34:38 +0000

    Add test for ranking applicant

--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -1,9 +1,26 @@
 from shortlister.tournament import get_pair, rank
 
 mylist = [
-    "apple", "banana", "bike", "bottle", "book", "car", "cat", "chair", 
-    "door", "dog", "laptop", "moon", "mountain", "pen", "phone", "river", 
-    "sun", "table", "tree", "window"
+    "apple",
+    "banana",
+    "bike",
+    "bottle",
+    "book",
+    "car",
+    "cat",
+    "chair",
+    "door",
+    "dog",
+    "laptop",
+    "moon",
+    "mountain",
+    "pen",
+    "phone",
+    "river",
+    "sun",
+    "table",
+    "tree",
+    "window",
 ]
 
 sample_result = {
@@ -32,12 +49,31 @@ sample_result = {
 
 def test_get_pair():
     result = get_pair(mylist)
-    expected = [(1, 2),(1, 3),(1, 4),(1, 5),(1, 6),(1, 7),(2, 3),(2, 4),(2, 5),(2, 6),(2, 7),(3, 4),(3, 5),(3, 6),(3, 7),(4, 5),(4, 6),(4, 7),(5, 6),(5, 7),(6, 7)]
     for item in result:
         assert result.count(item) == 1
-    assert result == expected
-
 
 
 def test_rank():
-    rank(mylist)
+    result = rank(mylist, sample_result)
+    assert result == [
+        "apple",
+        "bottle",
+        "book",
+        "dog",
+        "moon",
+        "bike",
+        "car",
+        "chair",
+        "door",
+        "laptop",
+        "phone",
+        "river",
+        "sun",
+        "tree",
+        "banana",
+        "cat",
+        "mountain",
+        "pen",
+        "table",
+        "window",
+    ]
