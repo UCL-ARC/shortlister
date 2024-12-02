346b561a9638fd0edc9544fbda3bfbc9b1cd0722
shiying <shiying.wei@ucl.ac.uk>
2024-12-02 13:44:41 +0000

    Add placeholder function for saving rank results

--- shortlister/tournament.py
+++ shortlister/tournament.py
@@ -1,26 +1,29 @@
 from itertools import combinations
 from typing import Dict, List
 
-RESULT= {}
+RESULT = {}
+
 
 def comparison(list):
     """Starts the comparison process"""
-    pairs = get_pair(list) # returns a list of pairs that can be used to compare
+    pairs = get_pair(list)  # returns a list of pairs that can be used to compare
 
-    for pair in pairs: 
+    for pair in pairs:
         winner = choose(pair)
-        save_results(pair,winner)
+        save_results(pair, winner)
     # goes to the next pair
+
 
 def get_pair(object_list):
     """Return every unique item pair in the list"""
     unique_pairs = list(combinations(object_list, 2))
-    pairs_to_compare =[pair for pair in unique_pairs if pair not in RESULT]
+    pairs_to_compare = [pair for pair in unique_pairs if pair not in RESULT]
     # then check against compared pairs in RESULTS
     # return not compared pairs
     return pairs_to_compare
 
-def choose(candidates:tuple):
+
+def choose(candidates: tuple):
     choice = input()
     if choice == "r":
         winner = candidates[0]
@@ -28,16 +31,19 @@ def choose(candidates:tuple):
         winner = candidates[1]
     return winner
 
-def save_results(pair,winner):
-    RESULT[set(pair)] = winner
 
-def rank(mylist:List,result:Dict):
+def save_results(pair, winner):
+    RESULT[tuple(pair)] = winner
+
+
+def rank(mylist: List, result: Dict):
+    """Rank applicants"""
     # checking from result where the winners should be placed
-    
+
     wins = {}
-    
+
     for object in mylist:
-    #award 1 point for every win
+        # award 1 point for every win
         outcome = list(result.values())
         score = outcome.count(object)
         wins[object] = score
@@ -46,9 +52,12 @@ def rank(mylist:List,result:Dict):
     return ranked
 
 
-    
+def save_rank(ranked_list):
+    """Save result to file"""
+    with open("ranked.csv") as file:
+        file.write(ranked_list)
 
-    
+
 # Ranking notes:
 
 # if object1 is winner, then it should be placed above object2 and all other objects that object2 won against ect.
