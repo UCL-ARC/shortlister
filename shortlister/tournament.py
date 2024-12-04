1dde38e781f7dc0b223054127d1e4a71739a4edb
shiying <shiying.wei@ucl.ac.uk>
2024-12-04 11:12:11 +0000

    Apply auto formatting

--- shortlister/tournament.py
+++ shortlister/tournament.py
@@ -3,35 +3,43 @@ from pathlib import Path
 import pickle
 from typing import Dict, List
 
-def comparison(mylist,result):
+
+def comparison(mylist, result):
     """Starts the comparison process"""
-    pairs = get_pair(mylist,result)  # returns a list of pairs that can be used to compare
+    pairs = get_pair(
+        mylist, result
+    )  # returns a list of pairs that can be used to compare
     for pair in pairs:
         print(f"{pair[0].name}[l] : {pair[1].name}[r]")
         winner = choose(pair)
-        save_results(pair, winner,result)
+        save_results(pair, winner, result)
     # goes to the next pair
 
-def get_pair(mylist,result):
+
+def get_pair(mylist, result):
     """Return every unique item pair in the list"""
-    unique_pairs = frozenset(combinations(mylist, 2)) # tuple pair is dependent on the list order
+    unique_pairs = frozenset(
+        combinations(mylist, 2)
+    )  # tuple pair is dependent on the list order
     pairs_to_compare = [pair for pair in unique_pairs if pair not in result]
     # then check against compared pairs in RESULTS
     # return uncompared pairs
     return pairs_to_compare
 
 
-def get_pair_ver2(mylist,result):
+# wip
+def get_pair_ver2(mylist, result):
     # assume list is ranked by score from high to low, start with the highest scored
     index = 0
     pair_index = 1
 
     # get a pair of first object with the object next in the list
-        # make the comparison, and the next pair will be the one that didnt win, and the next index
+    # make the comparison, and the next pair will be the one that didnt win plus the next index in the list
 
     while True:
-        pair = frozenset(mylist[index],mylist[pair_index])
-        try: 
+        pair = frozenset(mylist[index], mylist[pair_index])
+        winner = None
+        try:
             choice = input("r or l")
             if choice == "r":
                 winner = pair[1]
@@ -42,18 +50,15 @@ def get_pair_ver2(mylist,result):
         if winner == mylist[index]:
             index += 1
             pair_index += 1
-            pair = frozenset(mylist[index],mylist[pair_index])
-        elif winner == mylist[index+1]:
+            pair = frozenset(mylist[index], mylist[pair_index])
+        elif winner == mylist[index + 1]:
             pair_index += 1
-            pair = frozenset(mylist[index],mylist[pair_index])
-            return pair
-
-
+            pair = frozenset(mylist[index], mylist[pair_index])
 
 
 def choose(candidates: tuple):
     while True:
-        try: 
+        try:
             choice = input()
             if choice == "r":
                 winner = candidates[1]
@@ -63,8 +68,10 @@ def choose(candidates: tuple):
         except Exception:
             print("selection must be r or l")
 
-def save_results(pair, winner,result):
+
+def save_results(pair, winner, result):
     result[pair] = winner
+
 
 def rank(mylist: List, result: Dict):
     """Rank applicants"""
@@ -77,20 +84,23 @@ def rank(mylist: List, result: Dict):
         # award 1 point for every win
         score = outcome.count(object)
         wins[object] = score
-    
+
     ranked = sorted(mylist, key=lambda item: wins[item], reverse=True)
     save_rank(result)
     return ranked
 
-def save_rank(match_result,file:Path):
-    """Save dictionary of the comparison result to file"""
-    with open(file,"wb") as pickle_file:
-        pickle.dump(match_result,pickle_file)
 
-def open_existing_result(file:Path):
-    with open(file,"rb") as pickle_file:
+def save_rank(match_result, file: Path):
+    """Save dictionary of the comparison result to file"""
+    with open(file, "wb") as pickle_file:
+        pickle.dump(match_result, pickle_file)
+
+
+def open_existing_result(file: Path):
+    with open(file, "rb") as pickle_file:
         return pickle.load(pickle_file)
-    
+
+
 # Ranking notes:
 
 # if object1 is winner, then it should be placed above object2 and all other objects that object2 won against ect.
